7f14cc92a38a2e9f3507bb06bc6bf099893ddd04
Alex <howesalexandra@gmail.com>
2020-08-13 18:08:00 +0200

    Calculate the proportion of households with acces to electricity from the grid, enough drinking water, and child marriage, for 2018 and 2019.

--- analysis/counting/counting.py
+++ analysis/counting/counting.py
@@ -5,18 +5,22 @@ import sys
 plt.style.use("acaps")
 
 """
-Run some general counts/ bar charts of different properties against each other.
+Run some general counts/ bar charts of different properties against each other. Run counts for the 2018 and 2019 data, and compare. Save the results in a CSV file.
 """
+# Define indicators that can be compared between both datasets
+indicators = ["electricity_grid_score", "enough_water_drinking_cooking_washing", "child_marriage_score"]
+proportions = {"2018": {}, "2019": {}}
 
-"""
-Read in and process the data
-"""
-# Read in the data
-df = pd.read_csv("../../data/processed/MSNA_Host_2019.csv")
+# Loop through indicators and years
+for year in ["2018", "2019"]:
+    # Read in the data for that year
+    df = pd.read_csv("../../data/processed/MSNA_Host_"+year+".csv")
 
-"""
-Start some counting
-"""
-# Count household coping mechanisms for married and unmarried households
-df_married = df.loc[df["hoh_marital_married"]==1]
-df_not_married = df.loc[df["hoh_marital_married"]==0]
+    # Loop through indicators and calculate proportions (indicators are all 0/1)
+    for indicator in indicators:
+
+        df_indicator = df[indicator].dropna()
+        proportion = df_indicator.sum()/float(df_indicator.size)
+        proportions[year][indicator] = round(100*proportion)
+
+print(proportions)
