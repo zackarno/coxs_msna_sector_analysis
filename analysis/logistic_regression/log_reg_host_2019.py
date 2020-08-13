5663481c2fa25ef3bebdc8402f177dc03f027801
Alex <howesalexandra@gmail.com>
2020-08-13 13:28:33 +0200

    Add some extra steps into data processing to add new columns, etc. Add a logistic regression model to run logistic regression for a number of variables in the dataset. Use both the sklearn and statsmodels package, and print the results (including p-values and coefficients).

---
+++ analysis/logistic_regression/log_reg_host_2019.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import sys
+import math
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.utils import resample
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+import statsmodels.api as sm
+plt.style.use("acaps")
+
+"""
+Run a logistic regression on the host 2019 MSNA data.
+"""
+
+"""
+Read in and process the data
+"""
+# Read in the data
+df = pd.read_csv("../../data/processed/MSNA_Host_2019.csv")
+
+# Set the variables
+min_counts = None
+all_vars = {"categorical_vars": ["hoh_gender", "hoh_marital_married"],
+            "continuous_vars": ["hoh_age", "hh_size", "edu_highest_score"]}
+variables = {"hh_coping_mechanism.none": {"categorical_vars": all_vars["categorical_vars"],
+                                          "continuous_vars": all_vars["continuous_vars"]},
+            "feel_unsafe_female_yes_no": {"categorical_vars": all_vars["categorical_vars"],
+                                                      "continuous_vars": all_vars["continuous_vars"]},
+            "feel_unsafe_male_yes_no": {"categorical_vars": all_vars["categorical_vars"],
+                                                      "continuous_vars": all_vars["continuous_vars"]},
+            "enough_water_drinking_cooking_washing": {"categorical_vars": all_vars["categorical_vars"],
+                                                      "continuous_vars": all_vars["continuous_vars"]}}
+
+"""
+Loop through the dependent variables and run a logistic regression for each with different inputs.
+"""
+for dependent_variable in variables.keys():
+    df_log = df.copy()
+
+    """
+    Prepare the data for the model by selecting variables, converting categorical variables using one-hot encoding, dropping nans, and dropping the first column of categorical variables.
+    """
+    # Set the categorical and continuous variables
+    categorical_vars = variables[dependent_variable]["categorical_vars"]
+    continuous_vars = variables[dependent_variable]["continuous_vars"]
+
+    # Only keep rows with school age children if we are running the "Materiales de estudio" option
+    df_log = df_log[[dependent_variable]+categorical_vars+continuous_vars]
+    df_log = df_log.dropna(subset=([dependent_variable]+categorical_vars+continuous_vars), how="any")
+
+    # Remove categories with few datapoints
+    if min_counts != None:
+        for variable in (categorical_vars+continuous_vars):
+            counts = df_log[variable].value_counts()
+            df_log = df_log.loc[df_log[variable].isin(counts.index[counts >= min_counts].values)]
+
+    # Convert categorical data using one-hot encoding
+    df_log = pd.get_dummies(data=df_log, columns=categorical_vars, drop_first=False)
+    df_log = df_log.dropna()
+
+    # Drop an option from each category in order to avoid singularity
+    drop_firsts = {"hoh_gender": "male", "union_name": "Jalia Palong", "hoh_marital_married": 1}
+
+    for categorical_var in categorical_vars:
+        drop_column = categorical_var+"_"+str(drop_firsts[categorical_var])
+        df_log = df_log.drop(columns=[drop_column])
+
+    # Split into training and testing sets
+    X = df_log.loc[:, df_log.columns != dependent_variable]
+    y = df_log.loc[:, df_log.columns == dependent_variable]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    columns = X_train.columns
+
+    """
+    Run the model using the sklearn package
+    """
+    # Fit the model
+    clf = LogisticRegression(random_state=0).fit(X_train, y_train)
+
+    # Run predictions on the testing set and generate a score and confusion matrix
+    y_pred = clf.predict(X_test)
+    cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+    score = clf.score(X_test, y_test)
+
+    # Find the coefficients of the model
+    coefficients = pd.DataFrame(dict(zip(X_train.columns,clf.coef_[0])),index=[0]).T
+    coefficients = coefficients.rename(columns={0:"Coefficients"}).sort_values(by="Coefficients", ascending=False)
+
+    """
+    Run the model using the statsmodels package
+    """
+    # Train and fit the model
+    logit_model = sm.Logit(y_train, sm.add_constant(X_train))
+    result = logit_model.fit()
+
+    # Write the results summary to a file
+    summary = result.summary()
+    print(dependent_variable)
+    print(len(df_log))
+    print(summary)
+    #f = open("results/"+dependent_variable.replace(" ","_")+"_summary.txt", "w")
+    #f.write(str(summary))
+    #f.close()
+
+    # Extract the significant coefficients
+    pvalues = result.pvalues.reset_index().rename(columns={"index":"parameter", 0:"pvalues"})
+    coeffs = result.params.reset_index().rename(columns={"index":"parameter", 0:"coeffs"})
+    df_results = coeffs.merge(pvalues, on="parameter")
+    df_results = df_results.loc[df_results["pvalues"]<=0.05]
+    df_results = df_results.reindex(df_results["coeffs"].abs().sort_values(ascending=False).index)
+    df_results[["coeffs", "pvalues"]] = df_results[["coeffs", "pvalues"]].round(2)
+    #df_results.to_csv("results/"+dependent_variable.replace(" ","_")+"results.csv", index=False)
+    print(df_results)
+    print()
--- data/process_data.py
+++ data/process_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sys
 
 """
 Process the data, ensure columns are tidy, convert dates to correct formats, etc.
@@ -11,12 +12,32 @@ df = df.dropna(how="all")
 survey_questions = [column for column in df.columns if (column not in ["UUID", "survey_date", "enum_gender", "upazila_name", "union_name", "informed_consent", "X_index"])]
 df = df.dropna(how="all", subset=survey_questions)
 
-# Set head of household gender where it is given as N/A
-df.loc[df["hoh_gender"].isnull(), "hoh_gender"] = df["respondent_gender"]
+# Set head of household gender and age where it is given as N/A
+df.loc[(df["hoh_gender"].isnull()) & (df["respondent_hoh"] == "yes"), "hoh_gender"] = df["respondent_gender"]
+df.loc[(df["hoh_age"].isnull()) & (df["respondent_hoh"] == "yes"), "hoh_age"] = df["respondent_age"]
 
 # Create a new column for whether or not married
 df["hoh_marital_married"] = 0
 df.loc[df["hoh_marital"]=="married", "hoh_marital_married"] = 1
+
+# Tidy the edu_highest column to make it numeric for all answers
+df["edu_highest_score"] = df["edu_highest"]
+df.loc[df["edu_highest_score"]=="none", "edu_highest_score"] = 0
+df.loc[df["edu_highest_score"]=="above_12_tertiary_edu", "edu_highest_score"] = 13
+df.loc[df["edu_highest_score"]=="madrasah_only", "edu_highest_score"] = 1
+
+# Add a yes/ no column for the feel unsafe columns
+df["feel_unsafe_male_yes_no"] = 1
+df.loc[df["feel_unsafe_male"]=="none", "feel_unsafe_male_yes_no"] = 0
+df.loc[df["feel_unsafe_male"].isnull(), "feel_unsafe_male_yes_no"] = float("nan")
+df["feel_unsafe_female_yes_no"] = 1
+df.loc[df["feel_unsafe_female"]=="none", "feel_unsafe_female_yes_no"] = 0
+df.loc[df["feel_unsafe_female"].isnull(), "feel_unsafe_female_yes_no"] = float("nan")
+
+# Add a yes/ no column to the drinking water column
+df["enough_water_drinking_cooking_washing"] = 0
+df.loc[df["enough_water.dont_know"]==1, "enough_water_drinking_cooking_washing"] = float("nan")
+df.loc[(df["enough_water.drinking"]==1) & (df["enough_water.cooking"]==1) & (df["enough_water.washing_bathing"]==1), "enough_water_drinking_cooking_washing"] = 1
 
 # Save the processed data
 df.to_csv("./processed/MSNA_Host_2019.csv", index=False)
