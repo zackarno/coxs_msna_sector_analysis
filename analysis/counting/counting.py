4fbd65a69df31962e380aa5e5edb4eb1789a236b
Alex <howesalexandra@gmail.com>
2020-08-14 10:56:02 +0200

    Add an extension to divide by gender of the head of household.

--- analysis/counting/counting.py
+++ analysis/counting/counting.py
@@ -9,9 +9,9 @@ Run some general counts/ bar charts of different properties against each other. 
 """
 # Define indicators that can be compared between both datasets
 indicators = ["electricity_grid_score", "enough_water_drinking_cooking_washing", "child_marriage_score"]
-proportions = {"2018": {}, "2019": {}}
 
 # Loop through indicators and years
+proportions = {"2018": {}, "2019": {}}
 for year in ["2018", "2019"]:
     # Read in the data for that year
     df = pd.read_csv("../../data/processed/MSNA_Host_"+year+".csv")
@@ -24,3 +24,27 @@ for year in ["2018", "2019"]:
         proportions[year][indicator] = round(100*proportion)
 
 print(proportions)
+print()
+
+# Do the same again but split for male and female headed households
+proportions = {"2018": {}, "2019": {}}
+for year in ["2018", "2019"]:
+    # Read in the data for that year
+    df = pd.read_csv("../../data/processed/MSNA_Host_"+year+".csv")
+
+    # Loop through indicators and calculate proportions (indicators are all 0/1)
+    for indicator in indicators:
+
+        # Split by male and female headed households
+        proportions_gender = {}
+        for gender in ["male", "female"]:
+
+            df_gender = df.loc[df["hoh_gender"]==gender]
+            df_indicator = df_gender[indicator].dropna()
+            proportion = df_indicator.sum()/float(df_indicator.size)
+            proportions_gender[gender] = round(100*proportion)
+
+        proportions[year][indicator] = proportions_gender
+
+print(proportions)
+print()
